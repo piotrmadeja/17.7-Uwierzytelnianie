921d556d79fd6f16230125836d87ee4b4c9895ca
Piotr Madeja <piotr.madeja19@gmail.com>
2018-06-02 20:14:20 +0200

    17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':'889818005390-95uk1tjr327a70m622c3i2s0tq6it7ah.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET':'uev1ZaDviSXN2mYZPa3MH4PM',
+  'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
